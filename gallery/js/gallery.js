b2fdc52e7604fc084555ff340ee3b606006c9019
rom-dos <morera.computer@gmail.com>
2018-09-12 02:11:00 -0700

    Finish the JS gallery

---
+++ gallery/js/gallery.js
@@ -0,0 +1,23 @@
+function activateGallery () {
+
+  let thumbnails = document.querySelectorAll('#gallery-thumbs img')
+  let mainImage = document.querySelector('#gallery-photo img')
+
+  let galleryInfo = document.querySelector('#gallery-info')
+  let title = galleryInfo.querySelector('.title')
+  let description = galleryInfo.querySelector('.description')
+
+  thumbnails.forEach(function (thumbnail) {
+    thumbnail.addEventListener('click', function () {
+      let newImageSrc = thumbnail.dataset.largeVersion
+      let newImageAlt = thumbnail.alt
+      mainImage.setAttribute('src', newImageSrc)
+      mainImage.setAttribute('alt', newImageAlt)
+      title.textContent = thumbnail.dataset.title
+      description.textContent = thumbnail.dataset.description
+
+      document.querySelector('.current').classList.remove('current')
+      thumbnail.parentNode.classList.add('current')
+    })
+  })
+}
